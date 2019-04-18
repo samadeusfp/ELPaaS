6ba19953d0771f9deabb621d4c96cdb7abf4812f
MartinBauer <bauermax@informatik.hu-berlin.de>
2019-04-18 16:20:36 +0200

    added form fields for algorithm, pretsa params, mail adress

--- eventlogUploader/forms.py
+++ eventlogUploader/forms.py
@@ -1,7 +1,24 @@
 from django import forms
+from decimal import Decimal
 
 class DocumentForm(forms.Form):
     docfile = forms.FileField(
         label='Select a file',
         help_text='max. 42 megabytes'
     )
+
+    algorithm = forms.ChoiceField(
+        choices=(("1","PretSa"),("2","Other"),)
+    )
+    
+    t = forms.DecimalField(
+        label='Select t'
+        )
+
+    k = forms.IntegerField(
+        label='Select k'
+        )
+    
+    email = forms.EmailField(
+        label='Email adress'
+        )
