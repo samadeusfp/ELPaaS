56150b1504972136b3d9e3e2a5b8edce2049613d
Stephan Fahrenkrog-Petersen <samadeusfp@gmail.com>
2019-06-21 14:00:24 +0200

    Lifting some assumptions the input files

--- algorithms/PRETSA/runPretsa.py
+++ algorithms/PRETSA/runPretsa.py
@@ -13,17 +13,18 @@ t = sys.argv[3]
 dbName = sys.argv[4]
 secure_token = sys.argv[5]
 sys.setrecursionlimit(3000)
-print("Test123")
+filePath = filePath.replace(" ","_")
 if filePath.endswith(".xes"):
     log = xes_import_factory.apply(filePath)
     filePath = filePath + ".csv"
-    print(filePath)
     csv_exporter.export(log, filePath)
 targetFilePath = filePath.replace(".csv","_t%s_k%s_pretsa.csv" % (t,k))
 #run PRETSA
 if filePath.endswith(".xes.csv"):
     xes_csv_file = pd.read_csv(filePath, delimiter=",",skipinitialspace=True, encoding="utf-8-sig")
     xes_csv_file.rename(columns={'concept:name': 'Activity', 'case:concept:name': 'Case ID'}, inplace=True)
+    if not 'Duration' in xes_csv_file.columns:
+        xes_csv_file.loc[:,"Duration"] = 0.0
     xes_csv_file.to_csv(filePath,sep=";",encoding="utf-8-sig",index=False)
 eventLog = pd.read_csv(filePath, delimiter=";",skipinitialspace=True, encoding="utf-8-sig")
 pretsa_alg = pretsa.Pretsa(eventLog)
