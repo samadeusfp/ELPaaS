eb1a60c916ca38b6783cdc3b5420e1e3990ce507
MartinBauer <bauermax@informatik.hu-berlin.de>
2019-06-21 12:20:55 +0200

    status of file is changed to ERROR if expections occur during processing

--- algorithms/Laplacian_tv/runLaplacian_tv.py
+++ algorithms/Laplacian_tv/runLaplacian_tv.py
@@ -5,75 +5,69 @@ import subprocess
 import requests
 import time
 
-f=open("debug","w+")
-f.write("Executing runLaplacian with params\n")
 filePath = sys.argv[1]
 epsilon = sys.argv[2]
 n = sys.argv[3]
 p = sys.argv[4]
 dbName = sys.argv[5]
 secure_token = sys.argv[6]
-f.write("CWD      :"+str(os.getcwd()+"\n"))
-f.write("FilePath :"+str(filePath)+"\n")
-f.write("Epsilon  :"+str(epsilon)+"\n")
-f.write("n        :"+str(n)+"\n")
-f.write("p        :"+str(p)+"\n")
-f.write("dbName   :"+str(dbName)+"\n")
-f.write("Token    :"+str(secure_token)+"\n")
 
-#preprocess file
-os.mkdir(secure_token)
-command = subprocess.Popen(["rscript",
-                            os.getcwd()+"/ProtectedLog/data/convert.R",
-                            str(filePath),
-                            str(secure_token)])
-command.communicate()
-f.write("CSV Files generated at "+str(os.getcwd)+"/"+str(secure_token)+"\n")
+try:
+    #preprocess file
+    os.mkdir(secure_token)
+    command = subprocess.Popen(["rscript",
+                                os.getcwd()+"/ProtectedLog/data/convert.R",
+                                str(filePath),
+                                str(secure_token)])
+    command.communicate()
+    if command.returncode != 1:
+        raise Error
 
+    #start pinq server
+    server = subprocess.Popen([
+        "ProtectedLog/bin/Release/ProtectedLog.exe",
+        str(secure_token)+"/activities.csv",
+        str(secure_token)+"/precedence.csv",
+        str(secure_token)+"/log-sequences.csv",
+        "100000"
+        ])
 
-#start pinq server
-server = subprocess.Popen([
-    "ProtectedLog/bin/Release/ProtectedLog.exe",
-    str(secure_token)+"/activities.csv",
-    str(secure_token)+"/precedence.csv",
-    str(secure_token)+"/log-sequences.csv",
-    "100000"
-    ])
-
-page = requests.get("http://localhost:1234/")
-timeout = 120
-f.write(str(page.status_code)+"\n")
-while not page.status_code == 200:
-    time.sleep(5)
-    timeout -=5
     page = requests.get("http://localhost:1234/")
-    f.write("Retrying connection: "+str(page.status_code)+"\n")
-    if timeout<=0:
-        f.write("Timeout while starting Server. Aborting process!")
-        f.close()
-        server.kill()
-        sys.exit()
-f.write("Server started")
+    timeout = 120
+    while not page.status_code == 200:
+        time.sleep(5)
+        timeout -=5
+        page = requests.get("http://localhost:1234/")
+        if timeout<=0:
+            server.kill()
+            raise InputError
 
-outPath = filePath.replace(".xes","_%s_%s_%s.xes" % (epsilon, n, p))
+    outPath = filePath.replace(".xes","_%s_%s_%s.xes" % (epsilon, n, p))
 
-#get privatized log files
-command = subprocess.Popen(["rscript",
-                            os.getcwd()+"/ProtectedLog/data/discovery.R",
-                            str(epsilon),
-                            str(n),
-                            str(p),
-                            str(os.getcwd())+"/"+str(secure_token)+"/",
-                            outPath]
-                           ,cwd=os.getcwd()+"/ProtectedLog/data/")
-command.communicate()
-server.kill()
-#write to db
-puffer,targetFile = outPath.split("media/")
-conn = sqlite3.connect(dbName)
-c = conn.cursor()
-c.execute("UPDATE eventlogUploader_document SET status = ?, docfile = ? WHERE token = ?", ("FINISHED", targetFile, secure_token))
-conn.commit()
-conn.close()
-f.write("Done")
-f.close()
+    #get privatized log files
+    command = subprocess.Popen(["rscript",
+                                os.getcwd()+"/ProtectedLog/data/discovery.R",
+                                str(epsilon),
+                                str(n),
+                                str(p),
+                                str(os.getcwd())+"/"+str(secure_token)+"/",
+                                outPath]
+                               ,cwd=os.getcwd()+"/ProtectedLog/data/")
+    command.communicate()
+    server.kill()
+    if command.returncode != 1:
+        raise InputError
+
+    #write to db
+    puffer,targetFile = outPath.split("media/")
+    conn = sqlite3.connect(dbName)
+    c = conn.cursor()
+    c.execute("UPDATE eventlogUploader_document SET status = ?, docfile = ? WHERE token = ?", ("FINISHED", targetFile, secure_token))
+    conn.commit()
+    conn.close()
+except:
+    conn = sqlite3.connect(dbName)
+    c = conn.cursor()
+    c.execute("UPDATE eventlogUploader_document SET status = ? WHERE token = ?", ("ERROR", secure_token))
+    conn.commit()
+    conn.close()
--- algorithms/PRETSA/runPretsa.py
+++ algorithms/PRETSA/runPretsa.py
@@ -3,33 +3,43 @@ import pandas as pd
 import sqlite3
 import os
 import pretsa
+import logging
 
 #set parameters
+
 filePath = sys.argv[1]
 k = sys.argv[2]
 t = sys.argv[3]
 dbName = sys.argv[4]
 secure_token = sys.argv[5]
-sys.setrecursionlimit(3000)
-targetFilePath = filePath.replace(".csv","_t%s_k%s_pretsa.csv" % (t,k))
-#run PRETSA
-eventLog = pd.read_csv(filePath, delimiter=";",skipinitialspace=True, encoding="utf-8-sig")
-pretsa_alg = pretsa.Pretsa(eventLog)
-cutOutCases = pretsa_alg.runPretsa(int(k),float(t))
-privateEventLog = pretsa_alg.getPrivatisedEventLog()
-privateEventLog.to_csv(targetFilePath, sep=";",index=False)
 
-#save to file, change name to new
+try:
+    sys.setrecursionlimit(3000)
+    targetFilePath = filePath.replace(".csv","_t%s_k%s_pretsa.csv" % (t,k))
+    #run PRETSA
+    eventLog = pd.read_csv(filePath, delimiter=";",skipinitialspace=True, encoding="utf-8-sig")
+    pretsa_alg = pretsa.Pretsa(eventLog)
+    cutOutCases = pretsa_alg.runPretsa(int(k),float(t))
+    privateEventLog = pretsa_alg.getPrivatisedEventLog()
+    privateEventLog.to_csv(targetFilePath, sep=";",index=False)
 
-#Change existing entry instead of writing a new one, maybe add start and end time
-#Add PRETSA result to django-model/sqlite
-puffer,targetFile = targetFilePath.split("media/")
-conn = sqlite3.connect(dbName)
-c = conn.cursor()
-c.execute("UPDATE eventlogUploader_document SET status = ?, docfile = ? WHERE token = ?", ("FINISHED", targetFile, secure_token))
+    #save to file, change name to new
 
-#print mail to user stating file is ready now?
+    #Change existing entry instead of writing a new one, maybe add start and end time
+    #Add PRETSA result to django-model/sqlite
+    puffer,targetFile = targetFilePath.split("media/")
+    conn = sqlite3.connect(dbName)
+    c = conn.cursor()
+    c.execute("UPDATE eventlogUploader_document SET status = ?, docfile = ? WHERE token = ?", ("FINISHED", targetFile, secure_token))
 
-#c.execute("INSERT INTO eventlogUploader_document(docfile, token, status) VALUES (?,?,?)",(targetFile,secure_token,"FINISHED"))
-conn.commit()
-conn.close()
+    #print mail to user stating file is ready now?
+
+    #c.execute("INSERT INTO eventlogUploader_document(docfile, token, status) VALUES (?,?,?)",(targetFile,secure_token,"FINISHED"))
+    conn.commit()
+    conn.close()
+except:
+    conn = sqlite3.connect(dbName)
+    c = conn.cursor()
+    c.execute("UPDATE eventlogUploader_document SET status = ? WHERE token = ?", ("ERROR", secure_token))
+    conn.commit()
+    conn.close()
