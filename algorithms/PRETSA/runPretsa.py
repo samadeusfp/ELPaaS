dfe5b02e6f656ba6e7b21c0f5520744f0d699687
MartinBauer <bauermax@informatik.hu-berlin.de>
2019-06-21 17:50:04 +0200

    minor

--- algorithms/PRETSA/runPretsa.py
+++ algorithms/PRETSA/runPretsa.py
@@ -1,4 +1,5 @@
 try:
+    import smtplib
     import sys
     import pandas as pd
     import sqlite3
@@ -34,12 +35,13 @@ try:
     cutOutCases = pretsa_alg.runPretsa(int(k),float(t))
     privateEventLog = pretsa_alg.getPrivatisedEventLog()
     privateEventLog.to_csv(targetFilePath, sep=";",index=False)
-    puffer,targetFile = outPath.split("media/")
+    puffer,targetFile = filePath.split("media/")
     conn = sqlite3.connect(dbName)
     c = conn.cursor()
     c.execute("UPDATE eventlogUploader_document SET status = ?, docfile = ? WHERE token = ?", ("FINISHED", targetFile, secure_token))
     conn.commit()
     conn.close()
+
 except:
     filePath = sys.argv[1]
     k = sys.argv[2]
