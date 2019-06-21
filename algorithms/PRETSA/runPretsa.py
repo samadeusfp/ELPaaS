1e59b3567280b1eaab36ae1cfeb2fb58dbcb264e
Stephan Fahrenkrog-Petersen <samadeusfp@gmail.com>
2019-06-21 12:03:56 +0200

    Enable Upload XES file to PRETSA

--- algorithms/PRETSA/runPretsa.py
+++ algorithms/PRETSA/runPretsa.py
@@ -3,6 +3,8 @@ import pandas as pd
 import sqlite3
 import os
 import pretsa
+from pm4py.objects.log.importer.xes import factory as xes_import_factory
+from pm4py.objects.log.exporter.csv import factory as csv_exporter
 
 #set parameters
 filePath = sys.argv[1]
@@ -11,8 +13,18 @@ t = sys.argv[3]
 dbName = sys.argv[4]
 secure_token = sys.argv[5]
 sys.setrecursionlimit(3000)
+print("Test123")
+if filePath.endswith(".xes"):
+    log = xes_import_factory.apply(filePath)
+    filePath = filePath + ".csv"
+    print(filePath)
+    csv_exporter.export(log, filePath)
 targetFilePath = filePath.replace(".csv","_t%s_k%s_pretsa.csv" % (t,k))
 #run PRETSA
+if filePath.endswith(".xes.csv"):
+    xes_csv_file = pd.read_csv(filePath, delimiter=",",skipinitialspace=True, encoding="utf-8-sig")
+    xes_csv_file.rename(columns={'concept:name': 'Activity', 'case:concept:name': 'Case ID'}, inplace=True)
+    xes_csv_file.to_csv(filePath,sep=";",encoding="utf-8-sig",index=False)
 eventLog = pd.read_csv(filePath, delimiter=";",skipinitialspace=True, encoding="utf-8-sig")
 pretsa_alg = pretsa.Pretsa(eventLog)
 cutOutCases = pretsa_alg.runPretsa(int(k),float(t))
